855108a65e70c2ed0a64f35cf21a868fbbbb0269
php 01 <php.01@penadinho.cec.ime.usp.br>
2018-01-19 19:52:37 -0200

    arquivo de logica

---
+++ Logica.php
@@ -0,0 +1,25 @@
+<?php
+
+// camel case
+$nota = 7;
+
+// +, -, *, /, %, **
+
+
+// operadores booleanos
+// <, >, <= , >=, ==, !=
+// ===, !== ?????
+// == e ===
+
+
+// operadores lógicos
+// and E or
+// && e ||
+
+// <=>
+
+// mrezende.github.io
+
+for ($i=0; $i < 10; $i++) {
+  echo ($i + 1) . PHP_EOL;
+}
